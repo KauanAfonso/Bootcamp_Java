e42b5cc5c069857e65d538aa85e7f665c7712a02
KauanAfonso <kauanafon3@gmail.com>
2025-05-29 21:32:46 -0300

    aula de binarios

---
+++ src/Operadores/OperadorBitwise.java
@@ -0,0 +1,12 @@
+package Operadores;
+
+public class OperadorBitwise {
+    public static void main(String[] args) {
+        int value1 = 6;
+        var binary1 = Integer.toBinaryString(value1);
+        System.out.printf("Primeiro número da operação %s (representação binária %s)", value1, binary1);
+
+
+
+    }
+}
